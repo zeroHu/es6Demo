f6b8c90089f0a4894828f38b0cfaab529699dcd2
jianxia <jianxia@xfz.cn>
2017-07-06 19:29:04 +0800

    add some

---
+++ src/es6-09.js
@@ -0,0 +1,8 @@
+let arraylike = {
+    '0': '12',
+    '1': '13',
+    '2': 'zero',
+    length: 3
+};
+let array = Array.from(arraylike);
+console.log(array)
---
+++ src/es6-10.js
@@ -0,0 +1,3 @@
+const sortby = (a,b) => a-b;
+let newarr = [1,10,3,9,2].sort(sortby)
+console.log(newarr)
